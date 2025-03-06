75a48be189a694616c930a251d6a3f6940687cfa
Sumit-bali <sumitbali06@gmail.com>
2025-03-06 23:03:59 +0530

    boolean.c

---
+++ boolean.c
@@ -0,0 +1,20 @@
+# my-codes
+#include <stdio.h>
+
+int main() {
+    int n,a[10];
+    printf("enter the value ");
+    scanf("%d",&n);
+   int count=0;
+    while(n>0)
+    {
+     a[count]=n%2;
+     n=n/2;
+     count++;
+    }
+    for(int j=count;j>=0;j--)
+    {
+        printf("%d",a[j]);
+    }
+    return 0;
+}
